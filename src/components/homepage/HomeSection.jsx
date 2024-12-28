19fb6e741d48190ae4f0ba09ce52f5e231036ad2
manyajain27 <jainmanya2701@gmail.com>
2024-12-28 17:53:23 +0530

    commit

--- src/components/homepage/HomeSection.jsx
+++ src/components/homepage/HomeSection.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, ArrowDown } from 'lucide-react';
 import { TypeAnimation } from 'react-type-animation'; // Import smoother typing animation
 import bgImage from '../../media/bali-bg.jpg'; // Import background image
+
+
+
 function HomeSection() {
   return (
-    <div className="relative h-screen">
-      {/* Background Image */}
+    
+    <div >
+      <div className="relative h-screen">
+        {/* Background Image */}
       <div className="absolute inset-0">
         <img
           src={bgImage}
           alt="Travel"
           className="w-full h-full object-cover"
         />
-        <div className="absolute inset-0 bg-black/70"></div>
+        <div className="absolute inset-0 bg-black/65"></div>
       </div>
+
+      
 
       {/* Content Section */}
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full flex items-center">
-        <div className="container1 max-w-3xl">
+        <div className="container1 max-w-3xl flex flex-col gap-4">
           {/* Typing Animation for Heading */}
           <h1 className="text-5xl md:text-7xl font-bold text-white mb-6 flex flex-col gap-2">
             <span>Craft Your Perfect</span>
@@ -26,11 +33,11 @@ function HomeSection() {
               <TypeAnimation
                 sequence={[
                   'Adventure.', // First word
-                  1000,       
+                  2000,       
                   'Journey.',  // Second word
-                  1000,       
+                  2000,       
                   'Experience.', // Third word
-                  1000,       
+                  2000,       
                 ]}
                 wrapper="span"
                 speed={50} // Typing speed (lower is slower)
@@ -51,7 +58,14 @@ function HomeSection() {
               Learn More
             </button>
           </div>
+        
+      
         </div>
+      </div>
+      </div>
+      <div className="scroll absolute flex flex-col items-center ">
+        <span className='scroll-text text-gray-300'>Scroll Below</span>
+        <ArrowDown className='down-arrow h-8 w-8 text-gray-300'/>
       </div>
     </div>
   );
